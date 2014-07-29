e0a6ba8721a931b7957377d86aa7909e33d7a27d
Xolani Mfuko & Dustin Lewis <student@epicodus.com>
2014-07-29 11:08:21 -0700

    passes first spec, converting 2 to binary

--- spec/binary_spec.rb
+++ spec/binary_spec.rb
@@ -1,3 +1,9 @@
 require('rspec')
-require('binary.rb')
+require('binary')
 
+describe('binary_convert') do
+  it('converts integer 2 to binary form: 10') do
+    binary_convert("2").should(eq(10))
+  end
+
+end
